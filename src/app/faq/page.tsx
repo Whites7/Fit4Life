00ad83ea0823ea30c89c181ba915947bf408a537
CodeUnknownDex <91790491+CodeUnknownDex@users.noreply.github.com>
2023-11-27 11:03:36 +0100

    header + footer

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from "next";
 
 const FaqPage: NextPage = () => {
-    
+
     return (
         <div>
             {/* Your content goes here */}
+            <h1 className="bg-red-500">Faq Page</h1>
         </div>
     );
 };
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,33 @@
 import { NextPage } from "next";
 
 const Footer: NextPage = () => {
+
+    const year = new Date().getFullYear();
     
     return (
-        <div>
-            {/* Your content goes here */}
-        </div>
+       
+
+<footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {year} <a href="https://localhost" className="hover:underline">Fit4Life</a>. All Rights Reserved.
+    </span>
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" className="hover:underline">Contact</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
     );
 };
 
