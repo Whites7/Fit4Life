fe057ab9ce80786909f945a60d8206c380489064
aangeliev <61513069+aangeliev@users.noreply.github.com>
2023-12-18 11:14:51 +0100

    Neue Änderungen
    Neue Änderungen

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import "../../styles/signup.css";
+
+
+const SignUp: NextPage = () => {
+    
+    return (
+        <div className="container">
+            <div className="con1">Container 1</div>
+            <div className="con2">Container 2</div>
+            <div className="con3">Container 3</div>
+        </div>
+    );
+};
+
+export default SignUp;
